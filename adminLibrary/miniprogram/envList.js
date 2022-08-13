5df12e067aa99379e2a477ba0970208d2f62a32e
LAPTOP-3PQL97BV\jun <3031153460@qq.com>
2022-08-13 08:16:42 +0800

    updata

--- adminLibrary/miniprogram/envList.js
+++
@@ -1,6 +0,0 @@
-const envList = [{"envId":"cloud1-5gnbg2zy6247a86d","alias":"cloud1"},{"envId":"train1-8gadsea976f8124d","alias":"train1"}]
-const isMac = false
-module.exports = {
-    envList,
-    isMac
-}
